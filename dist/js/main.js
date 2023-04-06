2c93f603065cb791134dd2bed512357022e164cc
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-04-06 11:19:44 -0400

    complete drag and drop functionality for w3 example

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,44 @@
+// W3 DRAG AND DROP EXAMPLE
+const boxes = document.querySelectorAll(".box");
+const texts = document.querySelectorAll(".drag-text");
+
+boxes.forEach(box => {
+    box.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+
+    box.addEventListener("drop", (event) => {
+        event.preventDefault();
+        const data = event.dataTransfer.getData("text/plain");
+        const selectClass = document.getElementById(data);
+        box.appendChild(selectClass);
+    })
+});
+
+texts.forEach(text => {
+    text.addEventListener("dragstart", (event) => {
+        event.dataTransfer.setData("text/plain", event.target.id);
+    })
+})
+
+
+// MULTIPLE CHOICE EXAMPLE
 const options = document.querySelectorAll('.option');
 const dropzone = document.querySelector('.dropzone');
+const canada = document.getElementById('canada');
+
+canada.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("text/plain", event.target.id);
+    event.dataTransfer.setDragImage(event.target, 0, 0);
+});
+
+dropzone.addEventListener("dragover", (event) => {
+    event.preventDefault();
+});
+
+dropzone.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text/plain");
+    const element = document.getElementById(data);
+    dropzone.appendChild(element);
+});
