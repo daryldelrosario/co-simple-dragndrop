e3ae26665d0508fecf0dd6e6276b7f37457e7019
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-04-06 19:11:22 -0400

    refactor js to imply DRY methodology

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,34 +27,26 @@ const optionBoxes = document.querySelectorAll(".option-box");
 const optionTexts = document.querySelectorAll(".option-text");
 const answerBox = document.querySelector("#answer-box");
 
-optionBoxes.forEach(box => {
-    box.addEventListener("dragover", (event) => {
-        event.preventDefault();
-    });
+let prevBoxId;
 
-    box.addEventListener("drop", (event) => {
-        event.preventDefault();
-        const data = event.dataTransfer.getData("text/plain");
-        const element = document.getElementById(data);
-      
-        if (box.id === "answer-box") {
-          if (answerBox.childElementCount > 0) {
-            answerBox.replaceChild(element, answerBox.children[0]);
-          } else {
-            answerBox.appendChild(element);
-          }
-        } else {
-          box.appendChild(element);
-        }
-      });
+answerBox.addEventListener("dragover", (event) => {
+    event.preventDefault();
+});
+
+answerBox.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text/plain");
+    const element = document.getElementById(data);
+    if(answerBox.childElementCount > 0) {
+        answerBox.replaceChild(element, answerBox.children[0]);
+    } else {
+        answerBox.appendChild(element);
+    }
 });
 
 optionTexts.forEach(text => {
     text.addEventListener("dragstart", (event) => {
         event.dataTransfer.setData("text/plain", event.target.id);
+        prevBoxId = "#" + event.target.id + "-box";
     });
 });
-
-
-
-
