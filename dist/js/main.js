c2d31f4df10a8bc5017ed27537cadacf305925f8
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-04-06 17:03:02 -0400

    revert back to main javascript

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,47 +27,33 @@ const optionBoxes = document.querySelectorAll(".option-box");
 const optionTexts = document.querySelectorAll(".option-text");
 const answerBox = document.querySelector("#answer-box");
 
-let previousBox = null;
-
 optionBoxes.forEach(box => {
-  box.addEventListener("dragover", (event) => {
-    event.preventDefault();
-  });
+    box.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
 
-  box.addEventListener("drop", (event) => {
-    event.preventDefault();
-    const data = event.dataTransfer.getData("text/plain");
-    const element = document.getElementById(data);
-
-    if (box.id === "answer-box") {
-      if (answerBox.childElementCount > 0) {
-        const answerElement = answerBox.children[0];
-        answerBox.replaceChild(element, answerElement);
-        if (previousBox !== null) {
-          previousBox.appendChild(answerElement);
+    box.addEventListener("drop", (event) => {
+        event.preventDefault();
+        const data = event.dataTransfer.getData("text/plain");
+        const element = document.getElementById(data);
+      
+        if (box.id === "answer-box") {
+          if (answerBox.childElementCount > 0) {
+            answerBox.replaceChild(element, answerBox.children[0]);
+          } else {
+            answerBox.appendChild(element);
+          }
         } else {
-          document.getElementById(answerElement.getAttribute("data-start-id")).appendChild(answerElement);
+          box.appendChild(element);
         }
-      } else {
-        answerBox.appendChild(element);
-      }
-      previousBox = box.parentElement;
-      element.setAttribute("data-start-id", box.parentElement.id);
-    } else {
-      box.appendChild(element);
-      if (previousBox !== null && previousBox.id === "answer-box") {
-        previousBox.appendChild(document.getElementById(previousBox.children[0].getAttribute("data-start-id")));
-      }
-    }
-  });
+      });
 });
 
 optionTexts.forEach(text => {
-  text.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("text/plain", event.target.id);
-  });
+    text.addEventListener("dragstart", (event) => {
+        event.dataTransfer.setData("text/plain", event.target.id);
+    });
 });
-
 
 
 
