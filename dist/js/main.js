80aef6d236a9c81510e390737db87701116e2866
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-04-06 14:05:48 -0400

    complete semi-function with bugs drag and drop api for multiple choice example

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,34 +26,46 @@ texts.forEach(text => {
 const optionBoxes = document.querySelectorAll(".option-box");
 const optionTexts = document.querySelectorAll(".option-text");
 const answerBox = document.querySelector("#answer-box");
-const hello = "this is the new branch";
 
 optionBoxes.forEach(box => {
-    box.addEventListener("dragover", (event) => {
-        event.preventDefault();
-    });
+  box.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
 
-    box.addEventListener("drop", (event) => {
-        event.preventDefault();
-        const data = event.dataTransfer.getData("text/plain");
-        const element = document.getElementById(data);
-      
-        if (box.id === "answer-box") {
-          if (answerBox.childElementCount > 0) {
-            answerBox.replaceChild(element, answerBox.children[0]);
-          } else {
-            answerBox.appendChild(element);
-          }
-        } else {
-          box.appendChild(element);
-        }
-      });
-      
+  box.addEventListener("drop", (event) => {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text/plain");
+    const element = document.getElementById(data);
+    const sourceParent = element.parentNode;
     
+    if (box.id === "answer-box") {
+      if (answerBox.contains(element)) {
+        return;
+      }
+
+      if (answerBox.childElementCount > 0) {
+        const existingElement = answerBox.children[0];
+        existingElement.parentNode.removeChild(existingElement);
+        sourceParent.appendChild(existingElement);
+        answerBox.replaceChild(element, answerBox.children[0]);
+      } else {
+        answerBox.appendChild(element);
+      }
+    } else {
+      if (box.contains(element)) {
+        return;
+      }
+
+      box.appendChild(element);
+    }
+  });
 });
 
 optionTexts.forEach(text => {
-    text.addEventListener("dragstart", (event) => {
-        event.dataTransfer.setData("text/plain", event.target.id);
-    });
+  text.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("text/plain", event.target.id);
+  });
 });
+
+
+
